c5a661046f588c4ca0f5151cef5a44d6fbb24ef6
aungmoemy <72740033+aungmoemy@users.noreply.github.com>
2020-10-12 22:29:16 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-apt updatr && apt upgrade
-apt install git -y
-apt install php -y
 clear
 #bagian warna
 merah="\033[31;1m"
@@ -17,17 +14,19 @@ printf "\n${putih}[${merah}+${putih}]pilihan= " gameo
 read gameo
 if [ $gameo = 1 ] || [ $gameo = 01 ];then
 echo '                       Clipclap.apk '
-sleep 1
 termux-open https://play.google.com/store/apps/details?id=com.sanhe.clipclaps
 sleep 1
 sh install.sh
 elif [ $gameo = 2 ] || [ $gameo = 02 ];then
 echo "                       Appcloner.apk"
-sleep 1
 termux-open https://drive.google.com/file/d/1-6IXdPhWQ0_NJ3WA2finz1lWG2_peY4S/view
 sleep 1
 sh install.sh
 elif [ $gameo = 0 ] || [ $gameo = 00 ];then
-echo '         Exit '
-sleep 1
+clear
 exit
+else
+printf "${merah}!!!! input salah atau tidak boleh kosong !!!!"
+sleep 1
+sh install.sh
+if
